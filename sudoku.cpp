a5eef9d1227bdf3c784ecd38241f56592cc3a52d
ikesou0427 <arumikan1234@gmail.com>
2020-05-28 23:32:42 +0900

    solveを代入可能な値のみ実行するように変更　速度改善

--- sudoku.cpp
+++ sudoku.cpp
@@ -1,9 +1,10 @@
 #include<bits/stdc++.h>
 using namespace std;
 using VVI = vector<vector<int>>;
-const int TABLE_SIZE = 9;
+
+const int TABLE_SIZE = 9; // 9 * 9のサイズ前提 
+const vector<int> CONVERT_LIST{0b0, 0b100000000, 0b10000000, 0b1000000, 0b100000, 0b10000, 0b1000, 0b100, 0b10, 0b1};
 bool isDiscovered = false;
-// 9 * 9のサイズ前提 そのうち改造
 
 void input(VVI &table) {
     // ここだけでしか使えないってのがあるが妥協...
@@ -17,35 +18,39 @@ void input(VVI &table) {
         }
     }
 }
-bool checkRow(VVI table, int row, int col, int value) {
-    for(int i = 0;i < row;i++) {
-        if(table[i][col] == value) return false;
+int checkRow(VVI table, int row) {
+    int ableRowFlg = 0b1111111111;
+    for(int i = 0;i < TABLE_SIZE;i++) {
+        if(table[row][i] != 0) {
+            ableRowFlg = ableRowFlg ^ CONVERT_LIST[table[row][i]];  
+        }
     }
-    for(int i = row + 1;i < TABLE_SIZE;i++) {
-        if(table[i][col] == value) return false;
-    }
-    return true;
+    return ableRowFlg;
 }
-bool checkCol(VVI table, int row, int col, int value) {
-    for(int i = 0;i < col;i++) {
-        if(table[row][i] == value) return false;
+int checkCol(VVI table, int col) {
+    int ableColFlg = 0b1111111111;
+    for(int i = 0;i < TABLE_SIZE;i++) {
+        if(table[i][col] != 0) {
+            ableColFlg = ableColFlg ^ CONVERT_LIST[table[i][col]];  
+        }
     }
-    for(int i = col + 1;i < TABLE_SIZE;i++) {
-        if(table[row][i] == value) return false;
-    }
-    return true;
+    return ableColFlg;
 }
-bool checkBox(VVI table, int row, int col, int value) {
+int checkBox(VVI table, int row, int col) {
     int upperLeftRowOfBow = row / 3 * 3;
     int upperLeftColOfBow = col / 3 * 3;
 
+    int ableBoxFlg = 0b1111111111;
     for(int i = upperLeftRowOfBow;i < upperLeftRowOfBow + 3;i++) {
         for(int j = upperLeftColOfBow;j < upperLeftColOfBow + 3;j++) {
-            if(table[i][j] == value) return false;
+            if(table[i][j] != 0) {
+                ableBoxFlg = ableBoxFlg ^ CONVERT_LIST[table[i][j]];
+            }
         }
     }
-    return true;
+    return ableBoxFlg;
 }
+
 void output(VVI table) {
     for(int i = 0;i < TABLE_SIZE;i++) {
         for(int j = 0;j < TABLE_SIZE;j++) {
@@ -55,57 +60,43 @@ void output(VVI table) {
     }
 }
 void solve(VVI table, int row, int col, int value) {
-    // for(int j = 0;j < TABLE_SIZE;j++) {
-    //     cout << table[0][j] << " ";
-    // }
-    // cout << endl;
-    
-    cout << "対象マス(" << row + 1 << "行 " << col + 1 << "列) 代入予定: " << value << "\n";
-    output(table);
-    cout << endl;
-    if(row >= TABLE_SIZE) {
-        output(table);
-        isDiscovered = true;
-    } else {
-        
-        if(table[row][col] == 0) {
-            if(!checkRow(table, row, col, value)) return;
-            if(!checkCol(table, row, col, value)) return;
-            if(!checkBox(table, row, col, value)) return;
-            table[row][col] = value;
-
-            col += 1; // 次の列へ移動
-            if(col == TABLE_SIZE) { // 現在の列が端の次の列なら次の行に移動
-                col = 0;
-                row++;
-            }
-        } else {
-            while(table[row][col] != 0) {
-                col += 1; // 次の列へ移動
-                if(col == TABLE_SIZE) { // 現在の列が端の次の列なら次の行に移動
-                    col = 0;
-                    row++;
-                }
-            }
+    if(table[row][col] == 0) {
+        table[row][col] = value;
+    }
+    while(table[row][col] != 0) {
+        col += 1; // 次の列へ移動
+        if(col == TABLE_SIZE) { // 現在の列が端の次の列なら次の行に移動
+            col = 0;
+            row++;
         }
+        if(row >= TABLE_SIZE) break;
+    }
 
+    if(row < TABLE_SIZE) {
+        int ableNumFlg = checkRow(table, row) & checkCol(table, col) & checkBox(table, row, col);
         for(int i = 1;i <= TABLE_SIZE;i++) {
+            if(!(ableNumFlg & (0b1000000000 >> i))) continue;
             if(isDiscovered) return;
             solve(table, row, col, i);
         }
-    } 
+    } else {
+        output(table);
+        isDiscovered = true;
+    }
 }
 int main() {
     VVI table(TABLE_SIZE, vector<int>(TABLE_SIZE));
     input(table);
 
     if(table[0][0] == 0) {
+        int ableNumFlg = checkRow(table, 0) & checkCol(table, 0) & checkBox(table, 0, 0);
         for(int i = 1;i <= TABLE_SIZE;i++) {
-            solve(table , 0, 0, i);
+            if(!(ableNumFlg & (0b1000000000 >> i))) continue;
+            if(isDiscovered) return 0;
+            solve(table, 0, 0, i);
         }
     } else {
         solve(table, 0, 0, table[0][0]);
     }
-   
     return 0;
 }
